76e598d30f61e0123385dc4ec1e18ff7ab1e9fd6
ancarg <52886137+ancarg@users.noreply.github.com>
2019-07-15 18:20:06 +0300

    Create seed.groovy

---
+++ seed.groovy
@@ -0,0 +1,25 @@
+import groovy.json.JsonSlurper
+import groovy.json.*
+import groovy.json.JsonSlurper
+import groovy.json.JsonBuilder
+
+def jsonSlurper2 = new JsonSlurper()
+
+//update the filepath with the path of your job.
+
+def jobs = new BufferedReader(new InputStreamReader(new FileInputStream( "/var/lib/jenkins/workspace/SeedJob/seed.json"),"UTF-8"))
+data2 = jsonSlurper2.parse(jobs)
+def allSJObs = data2.jobs as Object
+data2.jobs.each
+  {
+    println("Create Job $it.repositoryName")
+
+    var (i=1;i<=10;i++){
+      job("job->$i") {
+        steps{
+          shell('echo $JOB_NAME')
+        
+        }
+    } 
+  }
+}
