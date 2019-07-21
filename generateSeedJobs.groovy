2796cfa9db1958b4fda36ecd1c32e9602b7d2f0c
ancarg <52886137+ancarg@users.noreply.github.com>
2019-07-21 23:35:52 +0300

    Update generateSeedJobs.groovy

--- generateSeedJobs.groovy
+++ generateSeedJobs.groovy
@@ -7,38 +7,24 @@ def jsonSlurper2 = new JsonSlurper()
 
 //update the filepath with the path of your job.
 
-def jobs = new BufferedReader(new InputStreamReader(new FileInputStream( "/var/lib/jenkins/workspace/SeedJob/seed.json"),"UTF-8"))
+def jobs = new BufferedReader(new InputStreamReader(new FileInputStream( "/var/lib/jenkins/workspace/testSeedJob/generateSeedJobs.json"),"UTF-8"))
 data2 = jsonSlurper2.parse(jobs)
 def allSJObs = data2.jobs as Object
 data2.jobs.each
   {
-    println("Create Job $it.repositoryName")
+    println("Create Job $it.moduleName")
 
 // here starts create the job
-    job("$it.repositoryName") {
+    job("$it.moduleName") {
         description(' This is the Description of the job.')
   
         logRotator(5, 100, -1, -1)
-        parameters {
-            stringParam('Name', 'DefaultValue', 'This is a first parameter')
-            stringParam('Age', '30', 'This is a second parameter')
-          stringParam('test', '1233430', 'This is a second parameter')
-        }
         label('master')
     steps {
-        downstreamParameterized {
-            trigger('Project1') {
-                block {
-                    buildStepFailure('FAILURE')
-                    failure('FAILURE')
-                    unstable('UNSTABLE')
-                }
-                parameters {
-                    predefinedProp('Name', '$Name')
-                    predefinedProp('Age', '$Age')
-               }
-            }
-        }
+      shell('echo clone repository $it.repositoryName')
+      shell('echo build module $it.moduleName')
+      shell('echo store result in $it.storeLocation')
+      shell('echo send email notifications to $it.emailingList')
       }
     }
 
