18bd787f386c98618c43f46d446bc946ee504bfc
ancarg <52886137+ancarg@users.noreply.github.com>
2019-07-22 12:32:00 +0300

    Update generateSeedJobs.groovy

--- generateSeedJobs.groovy
+++ generateSeedJobs.groovy
@@ -7,11 +7,20 @@ def jsonSlurper2 = new JsonSlurper()
 
 //update the filepath with the path of your job.
 
+//def jobs = new BufferedReader(new InputStreamReader(new FileInputStream( "/var/lib/jenkins/workspace/testSeedJob/generateSeedJobs.json"),"UTF-8"))
+//data2 = jsonSlurper2.parse(jobs)
+//def allSJObs = data2.jobs as Object
+//data2.jobs.each
+
 def jobs = new BufferedReader(new InputStreamReader(new FileInputStream( "/var/lib/jenkins/workspace/testSeedJob/generateSeedJobs.json"),"UTF-8"))
-data2 = jsonSlurper2.parse(jobs)
-def allSJObs = data2.jobs as Object
-data2.jobs.each
-  {
+def map = jsonSlurper2.parseText(jobs)
+map = map.jobs.collect(){job->
+  job.moduleName
+  job.repositoryName
+}
+
+
+ map.each {
     println("Create Job $it.moduleName")
 
 // here starts create the job
